9fa48e72bc751a00d3507bf6f54f6b9a7258a814
Yury Shchegolev <cardiffc@yandex.ru>
2020-02-08 19:48:40 +0300

    MongoDB connected

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,9 +1,15 @@
+import java.util.Arrays;
+
 public enum Commands {
     ДОБАВИТЬ_МАГАЗИН,
     ДОБАВИТЬ_ТОВАР,
     ВЫСТАВИТЬ_ТОВАР,
     СТАТИСТИКА_ТОВАРОВ,
-    help,
+    help;
+
+    public static String[] getNames(Class<? extends Enum<?>> e) {
+        return Arrays.stream(e.getEnumConstants()).map(Enum::name).toArray(String[]::new);
+    }
 
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,92 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 import java.util.Scanner;
 
 public class Main {
+    private static String databaseName = "local";
+    private static String host = "127.0.0.1";
+    private static int port = 27017;
+    private static String shops = "shops";
+    private static String goods = "goods";
+
     public static void main(String[] args) {
+        MongoCollection<Document> shopsCollection = getCollection(shops);
+        MongoCollection<Document> goodsCollection = getCollection(goods);
+
         while (true) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Введите команду (help для получения списка команд): ");
             String command = scanner.nextLine();
 
-            if (command.equals(Commands.help.toString())) {
-                System.out.println(Commands.ДОБАВИТЬ_МАГАЗИН + "\n" + Commands.ДОБАВИТЬ_ТОВАР + "\n"
-                        + Commands.ВЫСТАВИТЬ_ТОВАР + "\n" + Commands.СТАТИСТИКА_ТОВАРОВ);
+            String[] newAction = command.split(" ");
+
+            if (!checkCommand(newAction[0])) {
+                getErrorMessage();
             }
 
-            if (command.equals(Commands.ДОБАВИТЬ_МАГАЗИН.toString())) {
-                addShop();
+            if (newAction[0].equals(Commands.help.toString())) {
+
+                if (newAction.length != 1) {
+                    getErrorMessage();
+                } else {
+                    getHelp();
+                }
             }
 
-            if (command.equals(Commands.ДОБАВИТЬ_ТОВАР.toString())) {
-                addGood();
+            if (newAction[0].equals(Commands.ДОБАВИТЬ_МАГАЗИН.toString())) {
+                if (newAction.length != 2) {
+                    getErrorMessage();
+                } else {
+                    addShop(newAction, shopsCollection);
+                }
             }
 
-            if (command.equals(Commands.ВЫСТАВИТЬ_ТОВАР.toString())) {
-                merchGood();
+            if (newAction[0].equals(Commands.ДОБАВИТЬ_ТОВАР.toString())) {
+                if (newAction.length != 3) {
+                    getErrorMessage();
+                } else {
+                    addGood();
+                }
             }
 
-            if (command.equals(Commands.СТАТИСТИКА_ТОВАРОВ.toString())) {
-                getStatistics();
+            if (newAction[0].equals(Commands.ВЫСТАВИТЬ_ТОВАР.toString())) {
+                if (newAction.length != 3) {
+                    getErrorMessage();
+                } else {
+                    merchGood();
+                }
+            }
+
+            if (newAction[0].equals(Commands.СТАТИСТИКА_ТОВАРОВ.toString())) {
+                if (newAction.length != 1) {
+                    getErrorMessage();
+                } else {
+                    getStatistics();
+                }
             }
         }
     }
-    private static void addShop() {
 
+    private static boolean checkCommand(String command) {
+        String[] possibleCommands = Commands.getNames(Commands.class);
+        for (int i = 0; i < possibleCommands.length ; i++) {
+            if (command.equals(possibleCommands[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    private static void getErrorMessage() {
+        System.out.println("Неверная команда!!!");
+    }
+    private static void addShop(String[] action, MongoCollection<Document> shops) {
+       Document newShop = new Document();
+       newShop.append("name", action[1]);
+       shops.insertOne(newShop);
     }
 
     private static void addGood() {
@@ -48,5 +103,19 @@ public class Main {
 
 
     }
+
+    private static void getHelp() {
+        System.out.println(Commands.ДОБАВИТЬ_МАГАЗИН + "\n" + Commands.ДОБАВИТЬ_ТОВАР + "\n"
+                + Commands.ВЫСТАВИТЬ_ТОВАР + "\n" + Commands.СТАТИСТИКА_ТОВАРОВ);
+    }
+
+    private static MongoCollection<Document> getCollection (String collection) {
+        MongoClient mongoClient = new MongoClient(host, port);
+        MongoDatabase database = mongoClient.getDatabase(databaseName);
+        MongoCollection<Document> newCollection = database.getCollection(collection);
+        newCollection.drop();
+        return newCollection;
+    }
+
 
 }
