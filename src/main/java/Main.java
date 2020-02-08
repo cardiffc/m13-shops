aa39aa7dfd8e0d1729393c4f185ed5127c9c535d
Yury Shchegolev <cardiffc@yandex.ru>
2020-02-08 22:53:24 +0300

    Adding a good to shop, but can't update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import java.util.Scanner;
 
@@ -15,6 +17,7 @@ public class Main {
     public static void main(String[] args) {
         MongoCollection<Document> shopsCollection = getCollection(shops);
         MongoCollection<Document> goodsCollection = getCollection(goods);
+
 
         while (true) {
             Scanner scanner = new Scanner(System.in);
@@ -48,7 +51,7 @@ public class Main {
                 if (newAction.length != 3) {
                     getErrorMessage();
                 } else {
-                    addGood();
+                    addGood(newAction, goodsCollection);
                 }
             }
 
@@ -56,7 +59,7 @@ public class Main {
                 if (newAction.length != 3) {
                     getErrorMessage();
                 } else {
-                    merchGood();
+                    merchGood(newAction, shopsCollection);
                 }
             }
 
@@ -84,17 +87,30 @@ public class Main {
         System.out.println("Неверная команда!!!");
     }
     private static void addShop(String[] action, MongoCollection<Document> shops) {
-       Document newShop = new Document();
-       newShop.append("name", action[1]);
-       shops.insertOne(newShop);
+        Document newShop = new Document()
+                .append("name", action[1]);
+        shops.insertOne(newShop);
     }
 
-    private static void addGood() {
-
-
+    private static void addGood(String[] action, MongoCollection<Document> goods) {
+        Document newGood = new Document()
+                .append("name", action[1])
+                .append("price", Integer.parseInt(action[2]));
+        goods.insertOne(newGood);
     }
 
-    private static void merchGood() {
+    private static void merchGood(String[] action, MongoCollection<Document> shops) {
+
+//        Bson filter = new Document("name", "Harish Taware");
+//        Bson newValue = new Document("salary", 90000);
+//        Bson updateOperationDocument = new Document("$set", newValue);
+//        collection.updateOne(filter, updateOperationDocument);
+
+        Bson filter = new Document("name", action[1]);
+        Bson newValue = new Document("good", action[2]);
+        Bson updater = new Document("$set", newValue);
+
+        shops.updateOne(filter, updater);
 
 
     }
